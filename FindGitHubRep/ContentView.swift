2da9fb1f70ce2b57cbed0afb8e33a8b95863bb58
Mitsuharu Emoto <mthr1982@gmail.com>
2021-11-24 13:44:04 +0900

    first commit

---
+++ FindGitHubRep/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FindGitHubRep
+//
+//  Created by Mitsuharu Emoto on 2021/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FindGitHubRep/FindGitHubRepApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindGitHubRepApp.swift
+//  FindGitHubRep
+//
+//  Created by Mitsuharu Emoto on 2021/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FindGitHubRepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
