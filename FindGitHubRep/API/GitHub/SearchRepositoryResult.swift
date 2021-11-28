5a76a3d4290fa5dd72116c0bb8cb0b28b836ec21
Mitsuharu Emoto <mthr1982@gmail.com>
2021-11-28 17:51:19 +0900

    add api

---
+++ FindGitHubRep/API/GitHub/SearchRepositoryResult.swift
@@ -0,0 +1,19 @@
+//
+//  Repositories.swift
+//  FindGitHubRep
+//
+//  Created by Mitsuharu Emoto on 2021/11/28.
+//
+
+import Foundation
+
+struct SearchRepositoryResult: Codable {
+    
+    let total: Int
+    let items: [Repository]
+    
+    enum CodingKeys: String, CodingKey {
+        case total = "total_count"
+        case items
+    }
+}
--- FindGitHubRep/ViewModel/CounterViewModel.swift
+++ FindGitHubRep/ViewModel/CounterViewModel.swift
@@ -14,17 +14,42 @@ final class CounterViewModel: ObservableObject, Initializable {
   
   @ReMVVM.State<RootState> private var state
   @ReMVVM.Dispatcher private var dispatcher
+    
+    var api: API = API()
   
   required init() {
     $state.map(selectCount).assign(to: &$count)
   }
   
-  public func increase(){
+
+    public func increase()  {
     dispatcher[CounterAction.increase]()
+      
+        
+        if #available(iOS 15.0, *) {
+            Task {
+                do{
+                   try await searchRepositories()
+                }catch{
+                    debugPrint(error)
+                }
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+        
+   
   }
   
   func decrease(){
     dispatcher[CounterAction.decrease]()
   }
+    
+    @available(iOS 15.0.0, *)
+    func searchRepositories()  async throws -> SearchRepositoryResult{
+        let result: SearchRepositoryResult = try await api.searchRepositories(keyword: "swift")
+        debugPrint("searchRepositories: total: \(result.total)")
+        return result
+    }
   
 }
