187f739715b9bd83666577e4e1668d9594a5ada8
Mitsuharu Emoto <mthr1982@gmail.com>
2022-03-19 11:14:34 +0900

    fix self and MainActor

--- FindGitHubRep/ViewModel/SearchRepositoryViewModel.swift
+++ FindGitHubRep/ViewModel/SearchRepositoryViewModel.swift
@@ -9,18 +9,18 @@ import Foundation
 import ReMVVMSwiftUI
 
 final class SearchRepositoryViewModel: ObservableObject, Initializable {
-    @Published private(set) var isRequesting: Bool = false
+    @Published private(set) var isRequesting = false
     @Published private(set) var keyword: String = ""
     @Published private(set) var page: Int = 0
     @Published private(set) var total: Int = 0
-    @Published private(set) var hasNext: Bool = false
+    @Published private(set) var hasNext = false
     @Published private(set) var items: [Repository] = []
     @Published private(set) var error: Error?
 
     @ReMVVM.State<RootState> private var state
     @ReMVVM.Dispatcher private var dispatcher
 
-    let api: API = API()
+    let api = API()
 
     required init() {
         $state.map(selectRepositoryIsRequesting).assign(to: &$isRequesting)
@@ -34,40 +34,50 @@ final class SearchRepositoryViewModel: ObservableObject, Initializable {
 
     public func requestRepositories(keyword: String) {
         logger.info("SearchListViewModel#requestRepositories keyword: \(keyword)")
-        Task {
+        Task { [weak self] in
+            guard let self = self else {
+                return
+            }
             if self.isRequesting {
                 return
             }
-            await fetch(keyword: keyword, page: 1)
+            await self.fetch(keyword: keyword, page: 1)
         }
     }
 
     public func loadMoreRepositories() {
         logger.info("SearchListViewModel#loadMoreRepositories")
-        Task {
+        Task { [weak self] in
+            guard let self = self else {
+                return
+            }
             if self.isRequesting || !self.hasNext {
                 return
             }
-            await fetch(keyword: self.keyword, page: self.page + 1)
+            await self.fetch(keyword: self.keyword, page: self.page + 1)
         }
     }
 
     public func fetch(keyword: String, page: Int) async {
         do {
-            DispatchQueue.main.async {
-                self.dispatcher[RepositoryAction.fetch(keyword: keyword, page: page)]()
+            await MainActor.run { [weak self] in
+                // この非同期関数のなかで、画面更新に関わるところなので MainActor でメインスレッドで実行する
+                self?.dispatcher[RepositoryAction.fetch(keyword: keyword, page: page)]()
             }
+
             // 未認証なので、リクエスト発火を抑える
             await api.delay(sec: 0.5)
             let result = try await api.searchRepositories(keyword: keyword, page: page)
 
-            DispatchQueue.main.async {
-                self.dispatcher[RepositoryAction.succeeded(result)]()
+            await MainActor.run { [weak self] in
+                // この非同期関数のなかで、画面更新に関わるところなので MainActor でメインスレッドで実行する
+                self?.dispatcher[RepositoryAction.succeeded(result)]()
             }
         } catch {
             logger.warning("error: \(error), localizedDescription: \(error.localizedDescription)")
-            DispatchQueue.main.async {
-                self.dispatcher[RepositoryAction.failed(error)]()
+            await MainActor.run { [weak self] in
+                // この非同期関数のなかで、画面更新に関わるところなので MainActor でメインスレッドで実行する
+                self?.dispatcher[RepositoryAction.failed(error)]()
             }
         }
     }
